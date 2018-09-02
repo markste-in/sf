dfe13820368fa0e1daa3aaa04bf55ddb176eeb4f
Tommy Markstein <mail@markste.in>
2018-09-01 20:20:18 -0700

    fix

--- sf/__init__.py
+++ sf/__init__.py
@@ -1,1 +1,5 @@
+import sys
+sys.path.append('../')
+
+from sf.earth import earth
 name = "sf"
--- sf/setup.py
+++ sf/setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A python package for earth, balloon and rocket physics",
     long_description="A python package for earth, balloon and rocket physics",
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/sys-fail/sf,
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
